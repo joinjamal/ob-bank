560df0a130e4b43f57813e9402e1ac921ae43f8b
Jamal Marwan <jamal.abdulhaq@gmail.com>
2026-06-27 10:13:09 +0400

    Fix Android title bar and gold icon

--- android/app/src/main/java/com/obbank/app/MainActivity.java
+++ android/app/src/main/java/com/obbank/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.obbank.app;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import androidx.appcompat.app.ActionBar;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -14,6 +15,11 @@ public class MainActivity extends BridgeActivity {
         getWindow().setNavigationBarColor(OB_BANK_NAVY);
         getWindow().getDecorView().setBackgroundColor(OB_BANK_NAVY);
         super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
 
         View webView = getBridge().getWebView();
         webView.setBackgroundColor(OB_BANK_NAVY);
