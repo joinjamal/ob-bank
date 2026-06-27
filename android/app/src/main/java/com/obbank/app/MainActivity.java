c29415a6f7e6daf22f8a8a5ff0f7d4a0f25e3dec
Jamal Marwan <jamal.abdulhaq@gmail.com>
2026-06-27 10:04:15 +0400

    Polish Android startup and icon

--- android/app/src/main/java/com/obbank/app/MainActivity.java
+++ android/app/src/main/java/com/obbank/app/MainActivity.java
@@ -1,5 +1,21 @@
 package com.obbank.app;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    private static final int OB_BANK_NAVY = Color.rgb(7, 17, 31);
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        getWindow().setStatusBarColor(OB_BANK_NAVY);
+        getWindow().setNavigationBarColor(OB_BANK_NAVY);
+        getWindow().getDecorView().setBackgroundColor(OB_BANK_NAVY);
+        super.onCreate(savedInstanceState);
+
+        View webView = getBridge().getWebView();
+        webView.setBackgroundColor(OB_BANK_NAVY);
+    }
+}
